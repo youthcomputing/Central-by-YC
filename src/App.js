4e7684c09aea6f22178a90ad9f58a4a1a5aff498
Shaaf <shaaf.babar@gmail.com>
2020-09-27 17:31:01 -0600

    Header test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-
-
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img alt="logo" />
-        <p>
-          Shaaf <code>src/App.js</code> and save to hmmm.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
-  );
+  )
 }
-
-export default App;
