c2d52ab96b3cf4a4abc137719305cbc03a214412
Heet Shah <heetshah@Heets-MacBook-Pro-4.local>
2020-09-27 20:57:29 -0600

    Navbar progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Header from './Partials/Header';
+import React from "react";
+import Header from "./Partials/Header";
+
 function App() {
   return (
     <div className="App">
       <Header />
     </div>
-  )
+  );
 }
 export default App;
--- src/Partials/Header.js
+++ src/Partials/Header.js
@@ -1,11 +1,62 @@
-import React from 'react';
-function Header() {
-    return(
-        <nav class="navbar navbar-dark bg-primary">
-            <div className="row col-12 d-flex justify-content-center text-white">
-            <span className="h3">Login</span>
-            </div>
-        </nav>
-    )
+import React from "react";
+import "./header.css";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar
+        collapseOnSelect
+        className="hello"
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand href="#home">
+          <img src="/logo.svg" width="30" height="30" alt="Central by YC" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {/* Search Bar */}
+          <Nav className="searchBar mx-auto">
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className=" mr-sm-2"
+              />
+            </Form>
+          </Nav>
+          {/* End of search bar */}
+          {/* Spacing */}
+          <Nav className="mr-auto"></Nav>
+
+          {/* Right algined items */}
+          <Nav>
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#Profile">Profile</Nav.Link>
+            <Nav.Link href="#Storee">Store</Nav.Link>
+            <Nav.Link href="#Icon">Icon</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Lol</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
-export default Header;
+
+export default App;
