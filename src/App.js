a8ebf0ffc2b4dd3f09f91e8d93fe7e32c2fe9d74
Heet Shah <HeetShah@Westwoods-iMac-5.local>
2020-09-29 09:59:51 -0600

    seeing if the other nav bar is better

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Route } from "react-router-dom";
 
 import Header from "./Partials/Header";
+import Header2 from "./Partials/Header2";
+
 import Login from "./Authentication/Login";
 
 import * as ROUTES from "./Constants/Routes";
@@ -11,6 +13,7 @@ function App() {
   return (
     <Router>
       <Header />
+      <Header2 />
       <Route exact path={ROUTES.HOME} />
       <Route path={ROUTES.LOGIN} component={Login} />
     </Router>
