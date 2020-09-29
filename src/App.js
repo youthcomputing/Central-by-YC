e6393b1dd96c6c0babe44302f95f2b70825be99c
Heet Shah <HeetShah@Westwoods-iMac-5.local>
2020-09-29 09:43:16 -0600

    pushing nav bar changes

--- src/App.js
+++ src/App.js
@@ -9,17 +9,11 @@ import * as ROUTES from "./Constants/Routes";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <Header/>
-    </div>
-=======
     <Router>
       <Header />
       <Route exact path={ROUTES.HOME} />
       <Route path={ROUTES.LOGIN} component={Login} />
     </Router>
->>>>>>> 257e4f3a60cb438abc2d745d502c27784b0a9f2c
   );
 }
 
