530dc2ba11c5321a173aec638cf4117bfd20509b
Heet Shah <HeetShah@Westwoods-iMac-5.local>
2020-09-30 11:50:44 -0600

    nav bar changes to searchbar and navlinks

--- src/Partials/Header.js
+++ src/Partials/Header.js
@@ -10,52 +10,68 @@ import Dropdown from 'react-bootstrap/Dropdown';
 function App() {
   return (
     <div className="App">
-      <Navbar
-        collapseOnSelect
-        className="hello"
-        expand="lg"
-        bg="dark"
-        variant="dark"
-      >
+      <Navbar className="hello" expand="sm" variant="dark">
+
+
+
+        {/* This is the logo in the NavBar */}
         <Navbar.Brand href="#home">
           <img src="yclogo.png" width="30" height="30" alt="Central by YC" />
         </Navbar.Brand>
+        {/* This is the logo in the NavBar */}
+
+
+
+        {/* This is the search bar */}  
+        <Nav>
+           <Form className="searchBar " inline align>
+             <FormControl
+               type="text"
+               placeholder="Search"
+               className=" mr-sm-2"
+             />
+           </Form>
+        </Nav>
+        {/* This is the search bar */}
+
+
+        {/* Responsive nav bar ?? */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+
+
         <Navbar.Collapse id="responsive-navbar-nav">
-          {/* Search Bar */}
-          <Nav className="searchBar mx-auto">
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className=" mr-sm-2"
-              />
-            </Form>
-          </Nav>
-          {/* End of search bar */}
-          {/* Spacing */}
+          
+
+
+          {/* Spacing between search bar and nav links*/}
           <Nav className="mr-auto"></Nav>
+          {/* Spacing between search bar and nav links*/}
+
+
 
           {/* Right algined items */}
-          <Nav className="fontLato">
-            
-            <Nav.Link className="navItems"  href="#Profile">Home</Nav.Link>
+          <Nav variant="pills" className="fontLato">
+            <Nav.Link className="navItems "  href="#Home">Home</Nav.Link>
             <Nav.Link className="navItems"  href="#Profile">Profile</Nav.Link>
             <Nav.Link className="navItems" href="#Store">Store</Nav.Link>
-            <Nav.Link href="#Icon">
-              
-                <DropdownButton id="dropdown-item-button" alignRight title={<img width="30" height="30" src="Profile Icon.png" alt="">
-              
-              </img>}>
-                  <Dropdown.ItemText>Account</Dropdown.ItemText>
-                  
-                  <Dropdown.Item>Logout</Dropdown.Item>
-                </DropdownButton>
-              </Nav.Link>
+          </Nav>
+          <Nav>
+
+
 
             {/* This is the dropdown menu */}
-            
-          </Nav>
+          <Nav.Link href="#Icon">
+              <DropdownButton id="dropdown-item-button" alignRight title={<img width="30" height="30" src="Profile Icon.png" alt=""></img>}>
+                <Dropdown.ItemText>Account</Dropdown.ItemText>
+                <Dropdown.Item>Logout</Dropdown.Item>
+              </DropdownButton>
+            </Nav.Link>
+            </Nav>
+
+
+
+
         </Navbar.Collapse>
       </Navbar>
     </div>
