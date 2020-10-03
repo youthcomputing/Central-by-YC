20501e4a328b8eb2e422e6e8854f4a7141a2732e
sparshpatel6165@gmail.com <sparshpatel6165@gmail.com>
2020-10-03 03:04:11 -0600

    layout

--- src/Partials/selectUser.js
+++ src/Partials/selectUser.js
@@ -3,15 +3,27 @@ import "./selectUser.css";
 
 
 
-function App() {
+function User() {
   return (
-    <div className="role">
-      <text>Select Your Role</text>
-
+    <div className="User">
+      <text className="role">Select Your Role</text>
+      
+      <box1 className="outisdeboxleft"></box1>
+      <box className="boxleft"></box>
+      <people className="people"><img src="imagei.png" width="70" height="70" alt="Central by YC" /> </people>
+    <texter className="texts">Attendee</texter>
+    <content1 className="text11">• Attend events posted by organizers <br></br><br></br>
+    • Earn virtual currency to win hprizes <br></br><br></br>
+    • Personalized profile</content1>
+    
+    <button className="button"></button>
+    <next1 className="next1">Next</next1>
+    
+    
     </div>
   
   
   );
 }
 
-export default App;
+export default User;
