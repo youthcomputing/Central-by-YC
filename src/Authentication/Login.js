de0ac3e8f8923ebc8602aa8edd5e03c63d1ec38e
sparshpatel6165@gmail.com <sparshpatel6165@gmail.com>
2020-10-04 14:57:51 -0600

    Error Fixed

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./login.css";
+import "./Login.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
