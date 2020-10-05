97a25edbf3004a57a18ffc703268d599fc69e522
sparshpatel6165@gmail.com <sparshpatel6165@gmail.com>
2020-10-05 00:29:02 -0600

    scaling

--- src/Partials/selectUser.js
+++ src/Partials/selectUser.js
@@ -11,7 +11,7 @@ function User() {
     
     <box1 className="outisdeboxleft"></box1>
     <box className="boxleft"></box>
-    <people className="people"><img src="imagei.png" width="70" height="70" alt="Central by YC" /> </people>
+    <people className="people"><img src="imagei.png" width="55" height="55" alt="Central by YC" /> </people>
     <texter className="texts">Attendee</texter>
     <content1 className="text11">• Attend events posted by organizers <br></br><br></br>
     • Earn virtual currency to win hprizes <br></br><br></br>
@@ -21,12 +21,12 @@ function User() {
     
 
     <left className="left"></left>
-    <textt className="textt">• Post new events for general users <br></br><br></br>
+    <textt className="textt">• Post new events for users <br></br><br></br>
 • Analysis dashboard regarding event information <br></br><br></br>
 • View other events in the community</textt>
     <box111 className="boxxx"></box111>
     <organizer className="organizer">Organizer</organizer>
-    <people className="multiple"><img src="multiple.png" width="70" height="70" alt="Central by YC" /> </people>
+    <people className="multiple"><img src="multiple.png" width="55" height="55" alt="Central by YC" /> </people>
     
     
     </div>
