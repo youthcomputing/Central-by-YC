10be6ab1a4e16017dca9238cd41c26d6059a0850
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-10-10 11:04:04 -0600

    added firebase context

---
+++ src/Contexts/FirebaseContext.js
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+import * as KEYS from "../Constants/Keys";
+
+const firebaseAuthConfig = {
+  apiKey: KEYS.FIREBASE_AUTH_API_KEY,
+};
+
+firebase.initializeApp(firebaseAuthConfig);
+const auth = firebase.auth();
+const persistence = firebase.auth.Auth.Persistence;
+
+export const FirebaseContext = createContext({ auth, persistence });
