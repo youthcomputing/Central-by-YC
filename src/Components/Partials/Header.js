178b897e26d2f84c08abbb44956a788dcdf52bc7
HeetShah <heetshah7642@gmail.com>
2020-10-12 19:16:11 -0600

    clean up nav bar

--- src/Components/Partials/Header.js
+++ src/Components/Partials/Header.js
@@ -14,8 +14,7 @@ import { NavLink } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar collapseOnSelect className="Box" expand="lg" variant="dark">
+      <Navbar   className="color container-fluid" expand="lg" variant="dark">
         {/* This is the logo in the NavBar */}
         <Navbar.Brand href="/">
           <img src="yclogo.png" width="30" height="30" alt="Central by YC" />
@@ -23,64 +22,48 @@ function App() {
         {/* This is the logo in the NavBar */}
 
         {/* This is the search bar */}
-        <Nav>
-          <Form className="searchBar " inline align>
+        
+          <Form className="searchBar " inline >
             <FormControl
               type="text"
               placeholder="Search..."
               className="mr-sm-2 "
             />
           </Form>
-        </Nav>
+       
         {/* This is the search bar */}
-
-    
-
-        {/* Responsive nav bar */}
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-
-          
+        
+        <Navbar.Toggle  aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
           {/* Spacing between search bar and nav links*/}
-          <Nav className="mr-auto"></Nav>
-          {/* Spacing between search bar and nav links*/}
+          <Nav className=" mr-auto "></Nav>
+            {/* Spacing between search bar and nav links*/}
+            {/* Right algined items */}
+            <Nav>
+            <NavLink className="navItems fontLato" activeClassName="active" to="/"  >Home</NavLink>
+            <NavLink className="navItems fontLato " activeClassName="active" to="/login">Login</NavLink>
+            <NavLink className="navItems fontLato" activeClassName="active" to="/selectUser">Store</NavLink>
 
-          {/* Right algined items */}
-          <Nav  className="fontLato "  >
-            <NavLink className="navItems" activeClassName="active" to="/"  >Home</NavLink>
-            <NavLink className="navItems " activeClassName="active" to="/login" >
-              Login
-            </NavLink>
-            <NavLink className="navItems" activeClassName="active" to="/selectUser" >
-              Store
-            </NavLink>
-          </Nav>
-          <Nav>
-
-
-            
             {/* This is the dropdown menu */}
-            <Nav.Link href="#Icon">
-              <DropdownButton
-                id="dropdown-item-button"
-                alignLeft
-                title={
-                  <img
-                    width="30"
-                    height="30"
-                    src="Profile Icon.png"
-                    alt=""
-                  ></img>
-                }
-              >
-                <Dropdown.ItemText>Account</Dropdown.ItemText>
-                <Dropdown.Item>Logout</Dropdown.Item>
-              </DropdownButton>
-            </Nav.Link>
+            <DropdownButton
+            id="dropdown-item-button"
+            alignRight
+            title={
+              <img
+                width="30"
+                height="30"
+                src="Profile Icon.png"
+                alt=""
+              ></img>
+            }
+          >
+            <Dropdown.Item>Account</Dropdown.Item>
+            <Dropdown.Item>Logout</Dropdown.Item>
+          </DropdownButton>
           </Nav>
         </Navbar.Collapse>
+        
       </Navbar>
-    </div>
 
 
 
